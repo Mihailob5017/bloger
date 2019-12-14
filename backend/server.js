ce5181ce4094cebf2cfabed94894b2354f980129
Mihailo Boskovic <mihailob5017@gmail.com>
2019-12-15 00:13:09 +0100

    setting up the structure

---
+++ backend/server.js
@@ -0,0 +1,23 @@
+//requires
+const express = require("express");
+const cors = require("cors");
+const mongooDB = require("mongoose");
+require("dotenv/config");
+
+//starting app the server
+const port = process.env.PORT || 5000;
+const app = express();
+//middleware
+app.use(cors());
+app.use(express.json());
+
+
+
+//connecting to the database
+mongooDB.connect(process.env.DB_KEY, {useNewUrlParser: true,useUnifiedTopology: true}, () => {
+  console.log("MongooDb connected");
+});
+
+
+//start the server
+app.listen(port, () => console.log("Server Started!"));
